e2e0a3bc1aa505ec17dadc280c227f200fa3cdf6
gabrielle978 <gbassiquete@outlook.com>
2026-02-05 20:56:06 -0300

    creating first GUI / learning to create prompt alert

---
+++ javaFX1/src/main/java/br/com/dio/javafx1/GuiApplication.java
@@ -0,0 +1,23 @@
+package br.com.dio.javafx1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GuiApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GuiApplication.class.getResource("/br/com/dio/Gui.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 310);
+        stage.setTitle("testing GUI class");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
